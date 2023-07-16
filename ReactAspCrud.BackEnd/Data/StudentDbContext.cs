298b3e832d1655e0cde9a8ce24ed8fe3f279e66e
Nguyen Le Nguyen <82690801+nglenguyenn@users.noreply.github.com>
2023-07-16 09:40:20 +0700

    Add Backend

---
+++ ReactAspCrud.BackEnd/Data/StudentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactAspCrud.Models;
+
+namespace ReactAspCrud.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+        public DbSet<Student> Student { get; set; }
+    }
+}
--- ReactAspCrud.BackEnd/Program.cs
+++ ReactAspCrud.BackEnd/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ReactAspCrud.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add services for Sql Server
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
